180b75d58799a5ccbd38f71150cbf966178e4194
sansshete <sanskruti.satish2023@vitstudent.ac.in>
2025-08-31 15:52:17 +0530

    Updated config.ts

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 export const config = {
   // Server
-  port: parseInt(process.env.PORT || '3001'),
+  port: parseInt(process.env.PORT || '3000'),
   nodeEnv: process.env.NODE_ENV || 'development',
   
   // Database
